9d10cc740059e22d8feddd3cd91ef81f1f035f33
lucas.coelho <lucas.coelho@marketup.com>
2021-08-26 16:11:20 -0300

    Navigation implemented

---
+++ app/src/main/java/com/coelhoslab/android/appointmentapp/ui/activity/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.coelhoslab.android.appointmentapp.ui.activity.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.coelhoslab.android.appointmentapp.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
